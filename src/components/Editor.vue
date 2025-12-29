d171a04780b3bf0240a5be519527872230bfec5f
Captain Fulminyx <captainfulminyx@gmail.com>
2025-12-29 09:57:27 +0000

    try

---
+++ src/components/Editor.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+    <div>
+        <textarea></textarea>
+    </div>
+</template>
+<style scoped>
+textarea {
+    width: 300px;
+    height: 400px;
+    resize: none;
+    border: 3px solid #fff;
+    background: #000;
+    text:#fff;
+    outline: none;
+}
+</style>
